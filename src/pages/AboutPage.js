48637841e6ea44dbef0fa3e13bc814cad929eeb5
Heather <hlmathies@gmail.com>
2019-09-25 20:47:06 -0400

    inital commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <>
+    <h1>AboutPage</h1>
+    <p>
+      Sunt expetendis non deserunt, quem officia hic proident an quo sunt probant, sed
+      anim offendit possumus iis proident est commodo est litteris fore velit aliquip
+      esse e quid hic quamquam a tempor occaecat an domesticarum. Ipsum consectetur
+      offendit quis deserunt, minim nam ab tempor admodum, o qui quid ullamco, o esse
+      nisi quae officia an qui ut eram tamen cillum est ad veniam incurreret
+      mandaremus ex tamen vidisse se mandaremus ita ingeniis iis irure admodum.
+      Incurreret quem vidisse id offendit eram magna ex quae, laborum nisi eiusmod,
+      malis litteris est voluptate.
+    </p>
+    <p>
+      Labore quibusdam consequat. Et aliqua voluptate arbitrantur non hic ipsum
+      praesentibus ne est est philosophari quo legam sed mandaremus ab aute. Export
+      ullamco domesticarum, iudicem tamen quis nostrud dolor, id noster est quorum,
+      ubi iudicem praetermissum. A enim admodum arbitror, legam mandaremus ea
+      instituendarum ea irure iudicem iis occaecat, voluptate esse officia
+      cupidatat.Ne nulla expetendis distinguantur, de dolor comprehenderit.
+    </p>
+    <p>
+      Si ut cohaerescant, expetendis duis se arbitror despicationes, noster aliquip
+      expetendis, dolor nostrud nam distinguantur ut arbitror eu veniam doctrina, sed
+      ut instituendarum, sunt voluptate possumus ut cillum expetendis graviterque. Ex
+      dolore magna noster probant, est o export cupidatat, aute ullamco in eram
+      noster, laboris veniam summis cupidatat dolor, deserunt quorum dolor possumus
+      nisi, duis ne laboris, dolor consequat litteris aut si multos fore nam quamquam.
+    </p>
+  </>
+
+);
+
+export default AboutPage;
