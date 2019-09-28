e2c30963fdd36a3e4e86627a8971d29ee7a5cc27
Heather <hlmathies@gmail.com>
2019-09-28 08:47:34 -0400

    basic nav set up

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import {
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
+import NavBar from './NavBar';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact/>
           <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesList} />
-          <Route path="/article" component={ArticlePage} />
+          <Route path="/articles-list" component={ArticlesListPage} />
+          <Route path="/article/:name" component={ArticlePage} />
         </div>
+
       </div>
     </Router>
 
